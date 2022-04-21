8fc2cf174d452e10a78d00fd5553edfc8ec1c9c8
supervons <supervons@sina.com>
2022-04-21 22:12:25 +0800

    Change home page logo.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,41 +1,65 @@
-import React from 'react';
-import clsx from 'clsx';
-import Layout from '@theme/Layout';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import styles from './index.module.css';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
-import Translate from '@docusaurus/Translate';
+import React from "react";
+import clsx from "clsx";
+import Layout from "@theme/Layout";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import styles from "./index.module.css";
+import HomepageFeatures from "@site/src/components/HomepageFeatures";
+import Translate from "@docusaurus/Translate";
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   const images = [
-      {
-          alt: 'version',
-          imgSrc: 'https://img.shields.io/npm/v/react-native-echarts-pro.svg?style=flat',
-          jumpUrl: 'https://www.npmjs.com/package/react-native-echarts-pro'
-      },
-      {
-          alt: 'down-month',
-          imgSrc: 'https://img.shields.io/npm/dm/react-native-echarts-pro.svg?style=flat',
-          jumpUrl: 'https://www.npmjs.com/package/react-native-echarts-pro'
-      }
-  ]
+    {
+      alt: "version",
+      imgSrc:
+        "https://img.shields.io/npm/v/react-native-echarts-pro.svg?style=flat",
+      jumpUrl: "https://www.npmjs.com/package/react-native-echarts-pro",
+    },
+    {
+      alt: "down-month",
+      imgSrc:
+        "https://img.shields.io/npm/dm/react-native-echarts-pro.svg?style=flat",
+      jumpUrl: "https://www.npmjs.com/package/react-native-echarts-pro",
+    },
+  ];
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <header className={clsx("hero hero--primary", styles.heroBanner)}>
       <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        {
-          images.map(res=>{
-             return <img key={res.alt} style={{marginRight: 20, cursor: 'pointer'}} alt={res.alt} onClick={()=>{location.href=res.jumpUrl}} src={res.imgSrc}/>
-          })
-        }
+        <h1
+          className="hero__title"
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <img
+            style={{ height: "100px" }}
+            src={require("@site/static/img/logo.png").default}
+          />
+          {siteConfig.title}
+        </h1>
+        {images.map((res) => {
+          return (
+            <img
+              key={res.alt}
+              style={{ marginRight: 20, cursor: "pointer" }}
+              alt={res.alt}
+              onClick={() => {
+                location.href = res.jumpUrl;
+              }}
+              src={res.imgSrc}
+            />
+          );
+        })}
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
-              <Translate>Get Started️</Translate>
+            to="/docs/intro"
+          >
+            <Translate>Get Started️</Translate>
           </Link>
         </div>
       </div>
@@ -44,11 +68,12 @@ function HomepageHeader() {
 }
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
       title={`RNE-Pro docs`}
-      description="Description will go into a meta tag in <head />">
+      description="Description will go into a meta tag in <head />"
+    >
       <HomepageHeader />
       <main>
         <HomepageFeatures />
